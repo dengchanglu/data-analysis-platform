225ffafeaffbfe6def96f7e83fe9707775f4d038
IDluoyang <1126039485@qq.com>
2015-12-22 17:28:38 +0800

    luoyang[添加用户]

--- public/javascripts/controllers/indexCtr.js
+++ public/javascripts/controllers/indexCtr.js
@@ -30,5 +30,20 @@
         if ($scope.cookie_user == undefined || $scope.cookie_user == null || $scope.cookie_user == "") {
             $('#loginModal').modal('show');
         }
+
+        $scope.login=function(){
+            $('#loginModal').modal('show');
+
+        }
+        $scope.signIn=function(){
+            $('#loginModal').modal('hide');
+            alert('登录成功');
+           document.getElementById("userHeadImage").src='public/images/headImage.jpg'
+            var sl=$("#ss");
+            var ops=sl.find("option");
+            ops.eq(0).val("8").text("已登录").prop("selected",true);
+
+
+        }
     }
 })();
