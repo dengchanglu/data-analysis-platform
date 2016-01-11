0613458c788ed3e8081cd0d272b310bfa730b000
dengchanglu <1015701660@qq.com>
2016-01-11 17:14:06 +0800

    [dengchanglu]年龄分布分析

--- routes/controllers/controller.js
+++ routes/controllers/controller.js
@@ -31,5 +31,16 @@ app.get('/api/sexAnalysis', function (req, res) {
         res.send(data);
     });
 });
+/**
+ * 年龄分布分析
+ * 请求url示例 ： http://ip:端口/api/ageAnalysis?time=2016-01-11,2016-01-08
+ * 返回值示例： * 返回值示例：[{"key":"女","sex_count":56459},{"key":"未知","sex_count":56616},{"key":"男","sex_count":60468}]
+ */
+app.get('/api/ageAnalysis', function (req, res) {
+    var queryData = requestParsedUtil.getParamJson(req.url);
+    upAnalysis.upAnalysis.ageSearch(es.client, queryData, function (data) {
+        res.send(data);
+    });
+});
 
 module.exports = app;
