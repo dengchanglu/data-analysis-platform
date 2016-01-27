37cdd0076d15af98971f172aff5923b27ca3d46b
IDluoyang <1126039485@qq.com>
2016-01-27 18:18:09 +0800

    [luoyang]终端设备分页控件完善，表格分页完善，网络异常处理(页面动态已完成)

--- public/javascripts/controllers/versionDisCtr.js
+++ public/javascripts/controllers/versionDisCtr.js
@@ -67,7 +67,7 @@
                 }
                 $http.get(URL + "versionAnalysis?time=" + time + "&cm=" + cm + "&av=" + av).success(function (data) {
                     $scope.dataTable = [];
-                    console.log(data)
+
                     for (var o = 0; o < data[0].data.length; o++) {
                         var register_user = 0;
                         var active_user = 0;
@@ -93,6 +93,7 @@
                             start_count: start_count,
                             pre: (pre / pres * 100).toFixed(2)
                         });
+
 
                     }
 
