21348740b238d824a6ef942aff83130083ef05d0
dengchanglu <1015701660@qq.com>
2016-01-29 16:14:43 +0800

    [dengchanglu]访问页面的分析算法完成

--- public/javascripts/controllers/accessPageCtr.js
+++ public/javascripts/controllers/accessPageCtr.js
@@ -8,7 +8,30 @@
         .controller('accessPageCtr', accessPageCtr);
     function accessPageCtr($scope, $location, ipCookie, $rootScope) {
 
-
+        $scope.date = {
+            startDate: moment().subtract(1, "days"),
+            endDate: moment()
+        };
+        $scope.opts = {
+            locale: {
+                applyClass: 'btn-green',
+                applyLabel: "确定",
+                fromLabel: "Od",
+                toLabel: "Do",
+                cancelLabel: '取消',
+                customRangeLabel: '自定义时间',
+                daysOfWeek: ['六', '日', '一', '二', '三', '四', '五'],
+                firstDay: 1,
+                monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月',
+                    '十月', '十一月', '十二月'
+                ]
+            },
+            ranges: {
+                '7天': [moment().subtract(6, 'days'), moment()],
+                '14天': [moment().subtract(13, 'days'), moment()],
+                '30天': [moment().subtract(29, 'days'), moment()]
+            }
+        };
         require(
             [
                 'echarts',
@@ -25,7 +48,7 @@
                     },
                     tooltip: {
                         trigger: 'axis',
-                        axisPointer:{
+                        axisPointer: {
                             type: 'line',
                             lineStyle: {
                                 color: '#78C354',
@@ -133,7 +156,35 @@
 
                 TrendChart.setOption(option);
 
-            })
+            });
+        $scope.changeActive = function (type, id, number) {
+            for (var i = 0; i < number; i++) {
+                document.getElementById("page_btn_" + type + "_" + i).setAttribute("class", "btn btn-default");
+            }
+            document.getElementById(id).setAttribute("class", "btn btn-default active");
+        };
+        $scope.chooseVersion_page = function (av, id) {
+            $scope.changeActive("av", id, 7);
+        };
+        $scope.formatTime = function (time) {
+            var date = new Date(time);
+            return (date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate());
+        };
+        //Watch for date changes
+        $scope.$watch('date', function (newDate) {
+            var date1 = new Date($scope.formatTime(newDate.startDate));  //开始时间
+            var date2 = new Date($scope.formatTime(newDate.endDate));    //结束时间
+            var date3 = date2.getTime() - date1.getTime(); //时间差的毫秒数
+            //计算出相差天数
+            var days = Math.floor(date3 / (24 * 3600 * 1000));
+            var time = "";
+            for (var i = 0; i < days; i++) {
+                var dataTem = new Date(date1.getTime() + i * (24 * 3600 * 1000));
+                time += $scope.formatTime(dataTem) + ",";
+            }
+            time += $scope.formatTime(newDate.endDate);
+            //$scope.chooseCM($scope.templateCh, time, $scope.templateVersion.av);
+        }, false);
 
     }
 
